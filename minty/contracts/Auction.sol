15dc6c9d80e801672712716f075e96448b370794
wesley arizio <wesleyarizio@wesleys-MacBook-Pro.local>
2023-07-03 13:11:48 -0300

    add auction contract

---
+++ minty/contracts/Auction.sol
@@ -0,0 +1,137 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+
+contract Auction {
+    struct Listing {
+        IERC721 nft;
+        uint256 nftId;
+        uint256 minPrice;
+        uint256 highestBid;
+        address highestBidder;
+        uint256 endTime;
+        address owner;
+    }
+
+    uint256 nextListingId;
+    mapping(uint256 => Listing) listings;
+    mapping(address => uint256) balances;
+
+    event Bid(
+        address indexed bidder, 
+        uint indexed listingId, 
+        uint256 amount, 
+        uint256 timestamp
+    );
+    event List(
+        address indexed lister,
+        address indexed nft,
+        address indexed nftId,
+        uint256 listingId,
+        uint256 minPrice,
+        uint256 endTime,
+        uint256 timestamp
+    );
+
+    modifier listingExists(uint256 listingId) {
+        require(listings[listingId].owner != address(0), "Listing does not exist");
+        _;
+    }
+
+    modifier isOwner(uint256 nftId) {
+        require(nft.ownerOf(nftId) == msg.sender, "You do not own this nft");
+        _;
+    }
+
+    modifier hasNFTAccess(uint256 nftId) {
+        require(nft.getApproved(nftId) == address(this), "This contract is not approved to access this nft");
+        _;
+    }
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) public view returns (bytes4) {
+        // Called every time a nft is transfered using IERC721.safeTransferFrom;
+        return IERC721Receiver.onERC721Received.selector;
+    }
+
+    function end(uint256 listingId) external listingExists(listingId) {
+        Listing storage listing = listings[listingId];
+        require(block.timestamp > listing.endTime, "Auction is not over");
+        balances[listing.owner] += listing.highestBid;
+        listing.nft.safeTransferFrom(address(this), listing.highestBidder, listing.nftId);
+
+        delete listings[listingId];
+    }
+
+    function list(
+        IERC721 nft,
+        uint256 nftId,
+        uint256 minPrice,
+        uint256 numHours
+    ) isOwner(nftId) hasNFTAccess(nftId) {
+        nft.safeTransferFrom(msg.sender, address(this), nftId);
+
+        Listing storage listing = listings[nextListingId];
+        listing.nft = nft;
+        lising.nftId = nftId;
+        listing.minPrice = minPrice;
+        listing.endTime = block.timestamp + (numHours * 1 hours);
+        listing.owner = msg.sender;
+        listing.highestBidder = msg.sender;
+        listing.highestBidder = msg.sender;
+
+        emit List(
+            msg.sender,
+            nft,
+            nftId,
+            nextListingId,
+            minPrice,
+            listing.endTime,
+            block.timestamp
+        );
+
+        nextListingId++;
+    } 
+
+    function bid(uint256 listingId) external payable listingExists(listingId) {
+        Listing storage listing = listings[listingId];
+        require(msg.value >= listing.minPrice, "You must bid at least the min price");
+        require(msg.value > listing.highestBid, "You must bid higher than the current bid");
+        require(block.timestamp < listing.endTime, "Auction is over");
+
+        balances[listing.highestBidder] += listing.highestBid;
+        listing.highestBid = msg.value;
+        listing.highestBidder = msg.sender;
+
+        emit Bid(msg.sender, listingId, msg.value, block.timestamp);
+    }
+
+    function withdrawFunds() external {
+        uint256 balance = balances[msg.sender];
+        balances[msg.sender] = 0;
+        (bool sent, ) = payable(msg.sender).call{ value: balance }("");
+        require(sent, "Withdraw funds failed");
+    }
+
+    function getListing(uint256 listingId) public view listingExists(listingId) returns (
+        address,
+        uint256,
+        uint256,
+        uint256,
+        uint256
+    ) {
+        return (
+            address(listings[listingId].nft),
+            listings[listingId].nftId,
+            listings[listingId].highestBid,
+            listings[listingId].minPrice,
+            listings[listingId].endTime
+        );
+    }
+}
